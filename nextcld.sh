992e13b7e0dd0b37d73db2be663e91110d6d2851
glomidan <braux.danael@gmail.com>
2021-10-12 22:55:26 +0200

    patch nextcld.sh

--- nextcld.sh
+++ nextcld.sh
@@ -5,11 +5,20 @@
 echo -e "This script is written for \e[31mapt\e[0m package manager"
 
 echo -e "\e[34m############## \e[32mINSTALL SYSTEM REQUIREMENTS \e[34m##############\e[0m"
-echo "Update mirror list..."
-sudo apt update -y 2> /dev/null
+echo "Update mirror server list..."
+sudo apt update -y 1> /dev/null
 
-echo "Installing web server..."
-sudo apt install apache2 libapache2-mod-php php php-cgi php-mysql php-cli php-curl php-gd php-intl php-memcache php-xml php-zip php-mbstring php-json bzip2 wget mariadb-server -y
+echo "[   5% ] Installing web server..."
+sudo apt install apache2 libapache2-mod-php -y 1> /dev/null
+echo "[  15% ] Installing PHP and PHP services..."
+sudo apt install php php-cgi php-mysql php-cli php-curl php-gd -y 1>/dev/null
+echo "[  45% ] Installing PHP and PHP services..."
+sudo apt install php-intl php-memcache php-xml php-zip php-mbstring php-json -y 1>/dev/null
+echo "[  60% ] Installing tools..."
+sudo apt install bzip2 wget -y 1>/dev/null 
+echo "[  80% ] Installing database server..."
+sudo apt install mariadb-server -y 1>/dev/null
+echo "[ 100% ] Installing of services complete"
 
 echo -e "\e[34m################### \e[32mINSTALL NEXTCLOUD \e[34m###################\e[0m"
 echo "Downloading NextCloud"
@@ -17,7 +26,7 @@ cd /tmp
 wget https://download.nextcloud.com/server/releases/nextcloud-22.1.1.zip
 echo "Unpack files..."
 exit 0
-sudo unzip nextcloud-22.1.1.zip -d /var/www/html/ 2>/dev/null
+sudo unzip nextcloud-22.1.1.zip -d /var/www/html/ 1>/dev/null
 cd /var/www/html
 echo "Changing files's owner"
 sudo chown -R www-data:www-data nextcloud
@@ -40,20 +49,27 @@ FLUSH PRIVILEGES;" > ./db.sql
 
 sudo mysql -u root -p < ./db.sql
 rm ./db.sql
+psswd='\0'
 
 echo -e "\e[34m################## \e[32mENDING INSTALLATION \e[34m##################\e[0m"
-echo "To activating website"
 echo '<IfModule mod_alias.c>
-Alias /owncloud /var/www/html/nextcloud
+Alias /cloud /var/www/html/nextcloud
 </IfModule>
 <Directory "/var/www/html/nextcloud">
 Options Indexes FollowSymLinks
 AllowOverride All
 Require all granted
-</Directory>'
+</Directory>' > /etc/apache2/sites-enabled/nextcloud.conf
+
+echo '<VirtualHost *:80>
+    ServerName cloud
+    DocumentRoot /var/www/nextcloud
+</VirtualHost>' > /etc/apache2/sites-available/cloud
+
+clear
 
 echo "Restart web server..."
-sudo /etc/init.d/apach2.service restart
+sudo /etc/init.d/apache2.service restart
 systemctl status apache2.service
 
 echo -e "\e[34m################# \e[32mINSTALLATION COMPLETE  \e[34m################\e[0m"
